726685964fa82e949759ee2fe1175eb9fb5f8879
Vasile Marin <vmarin@c3r4s2.42london.com>
2024-04-17 17:44:44 +0100

    ft_strlcat solved

---
+++ ft_strlcat.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 17:11:09 by vmarin            #+#    #+#             */
+/*   Updated: 2024/04/17 17:44:26 by vmarin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	j;
+	size_t	src_len;
+	size_t	dst_len;
+
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
+	i = 0;
+	j = dst_len;
+	if (size > 0)
+	{
+		while (src[i] != '\0')
+		{
+			dst[j] = src[i];
+			j++;
+			i++;
+			if (j == size - 1)
+				break ;
+		}
+	}
+	dst[j] = '\0';
+	return (src_len + dst_len);
+}
