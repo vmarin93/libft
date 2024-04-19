0684d12a9caafb7fb3edae5f519107339a703026
Vasile Marin <vmarin@c3r4s9.42london.com>
2024-04-19 19:13:40 +0100

    ft_strdup solved

---
+++ ft_strdup.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 18:42:13 by vmarin            #+#    #+#             */
+/*   Updated: 2024/04/19 19:13:17 by vmarin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_strdup(const char *s)
+{
+	char	*duplicate;
+	size_t	i;
+
+	if (s == NULL)
+		return (NULL);
+	duplicate = malloc(sizeof(s));
+	if (duplicate == NULL)
+		return (NULL);
+	i = 0;
+	while (i < sizeof(s))
+	{
+		duplicate[i] = s[i];
+		i++;
+	}
+	return (duplicate);
+}
