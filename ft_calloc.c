bad6d57e8fe311364d5d525d3188b0f7ef792a2b
Vasile Marin <vmarin@c3r4s9.42london.com>
2024-04-19 18:40:11 +0100

    ft_calloc solved

---
+++ ft_calloc.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 16:08:37 by vmarin            #+#    #+#             */
+/*   Updated: 2024/04/19 18:39:51 by vmarin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*ptr;
+	size_t	i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		nmemb = 1;
+		size = 1;
+	}
+	ptr = malloc(nmemb * size);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < nmemb * size)
+	{
+		((char *)ptr)[i] = 0;
+		i++;
+	}
+	return ((void *)ptr);
+}
