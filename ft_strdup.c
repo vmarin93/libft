f2e2a9b01086be6d934aa3ba511223f4e0034956
Vasile Marin <vmarin@c3r4s10.42london.com>
2024-04-27 23:14:08 +0100

    changed sizeof to strlen to get correct amount of bytes to allocate to duplicate in strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,12 +6,13 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 18:42:13 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:38:07 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/04/27 23:13:09 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stddef.h>
+#include "libft.h"
 
 char	*ft_strdup(const char *s)
 {
@@ -20,11 +21,11 @@ char	*ft_strdup(const char *s)
 
 	if (s == NULL)
 		return (NULL);
-	duplicate = malloc(sizeof(s));
+	duplicate = malloc(ft_strlen(s) + 1);
 	if (duplicate == NULL)
 		return (NULL);
 	i = 0;
-	while (i < sizeof(s))
+	while (i < ft_strlen(s) + 1)
 	{
 		duplicate[i] = s[i];
 		i++;
