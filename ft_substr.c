511132dba5332a0018c9f8751b10f69d2765d1d9
Vasile Marin <vmarin@c3r4s10.42london.com>
2024-04-27 23:34:50 +0100

    fix bug where I was allocating len amount of memory before defining len...

--- ft_substr.c
+++ ft_substr.c
@@ -6,42 +6,13 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 19:16:38 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 22:03:17 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/04/27 23:33:51 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stddef.h>
 #include "libft.h"
-
-//size_t	ft_strlen(const char *str)
-//{
-//	size_t	i;
-//
-//	i = 0;
-//	while (str[i] != '\0')
-//		i++;
-//	return (i);
-//}
-//
-//char	*ft_strdup(const char *s)
-//{
-//	char	*duplicate;
-//	size_t	i;
-//
-//	if (s == NULL)
-//		return (NULL);
-//	duplicate = malloc(sizeof(s));
-//	if (duplicate == NULL)
-//		return (NULL);
-//	i = 0;
-//	while (i < sizeof(s))
-//	{
-//		duplicate[i] = s[i];
-//		i++;
-//	}
-//	return (duplicate);
-//}
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
@@ -54,11 +25,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	{
 		return (ft_strdup(""));
 	}
+	if (start + len > ft_strlen(s))
+		len = ft_strlen(s + start);
 	substr = malloc(len + 1);
 	if (substr == NULL)
 		return (NULL);
-	if (start + len > ft_strlen(s))
-		len = ft_strlen(&s[start]);
 	i = 0;
 	while (i < len)
 	{
