9a2cce74db362e756d81b790ffb5496d7d08a1c3
Vasile Marin <vmarin@c3r4s10.42london.com>
2024-04-27 18:02:30 +0100

    rewrite of strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,32 +6,37 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 15:46:38 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:38:29 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/04/27 18:00:53 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
+	size_t	src_len;
 
-	i = 0;
-	if (size > 0)
+	src_len = ft_strlen(src);
+	if (src_len + 1 < size)
 	{
-		while (src[i] != '\0')
+		i = 0;
+		while (i < src_len + 1)
 		{
-			if (i == size)
-			{
-				i--;
-				break ;
-			}
 			dst[i] = src[i];
 			i++;
 		}
 	}
-	dst[i] = '\0';
-	while (src[i] != '\0')
-		i++;
-	return (i);
+	else if (size != 0)
+	{
+		i = 0;
+		while (i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (src_len);
 }
