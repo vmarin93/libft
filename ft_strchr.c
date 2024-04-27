3468759b46c2e033ff6b66b99e9cbceec6763890
Vasile Marin <vmarin@c3r4s10.42london.com>
2024-04-27 14:19:23 +0100

    fix char to int bug and cast out of const for strchr function

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,20 +6,25 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 19:01:36 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:37:59 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/04/27 14:18:12 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
+#include "libft.h"
 
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
+	int	len;
+	char	to_find;
 
+	to_find = (char)c;
+	len = ft_strlen(s);
 	i = 0;
-	while (s[i] != '\0')
+	while (i < len + 1)
 	{
-		if (s[i] == c)
+		if (s[i] == to_find)
 			return ((char *)s + i);
 		i++;
 	}
