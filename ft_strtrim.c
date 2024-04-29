558425bf9e9c330a29caacc06dc26ba788a0f9f7
Vasile Marin <vasilemarin93@gmail.com>
2024-04-29 15:34:57 +0100

    strtrim solved

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -13,6 +13,7 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include "libft.h"
+#include <stdio.h>
 
 int	is_in_set(char const c, char const *set)
 {
@@ -30,26 +31,27 @@ int	is_in_set(char const c, char const *set)
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	start_index;
-	size_t	end_index;
-	char	*result;
+    char    *result;
+    size_t  start;
+    size_t  end;
+    size_t  i;
 
-	start_index = 0;
-	while (is_in_set(s1[start_index], set))
-		start_index++;
-	end_index = ft_strlen(s1);
-	while (is_in_set(s1[end_index], set))
-		end_index--;
-	result = malloc((end_index - start_index) * sizeof(char) + 1);
-	if (result == NULL)
-		return (NULL);
-	i = 0;
-	while (i <= end_index - start_index)
-	{
-		result[i] = s1[start_index + i];
-		i++;
-	}
-	result[i] = '\0';
-	return (result);
+    if (!s1 || !set)
+        return (NULL);
+    start = 0;
+    end = ft_strlen(s1);
+    while (s1[start] && is_in_set(s1[start], set))
+        start++;
+    while (s1[end - 1] && is_in_set(s1[end - 1], set))
+        end--;
+    if (start > end)
+        return (ft_strdup(""));
+    result = ft_calloc(sizeof(char), ((end - start) + 1));
+    i = 0;
+    while (start + i < end)
+    {
+        result[i] = s1[start + i];
+        i++;
+    }
+    return (result);
 }
