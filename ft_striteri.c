63e2d0fb47a884bb47d6ef3be7668bb9a98b7a01
Vasile Marin <vasilemarin93@gmail.com>
2024-04-30 00:45:43 +0100

    strmapi and striteri solved

---
+++ ft_striteri.c
@@ -0,0 +1,13 @@
+#include <stddef.h>
+
+void    ft_striteri(char *s, void (*f)(unsigned int, char *))
+{
+    size_t  i;
+
+    i = 0;
+    while (s[i] != '\0')
+    {
+        f(i, s + i);
+        i++;
+    }
+}
---
+++ ft_strmapi.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "libft.h"
+
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    char    *result;
+    size_t  i;
+
+    result = ft_calloc(ft_strlen(s) + 1, sizeof(char));
+    if (result == NULL)
+        return (NULL);
+    i = 0;
+    while (s[i] != '\0')
+    {
+        result[i] = f(i, s[i]);
+        i++;
+    }
+    return (result);
+}
