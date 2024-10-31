bd8b37b3b7b59a8d93e0f9619c1a5eb85559b79b
Vasile Marin <vasilemarin93@gmail.com>
2024-10-31 20:16:59 +0000

    changed ft_putnbr_fd to return nbr of chars written to stdout

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,35 +6,39 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:33:42 by vmarin            #+#    #+#             */
-/*   Updated: 2024/05/01 18:30:37 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/05/08 15:13:54 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-static void	do_putnbr(int n, int fd)
+static int	do_putnbr(int n, int fd, int *chars_printed)
 {
 	char	digit;
 
-	if (n == 0)
-		write(fd, "0", sizeof(char));
-	else if (n / 10 != 0)
+	if (n / 10 != 0)
 	{
-		do_putnbr(n / 10, fd);
-		do_putnbr(n % 10, fd);
+		do_putnbr(n / 10, fd, chars_printed);
+		do_putnbr(n % 10, fd, chars_printed);
 	}
 	else
 	{
 		digit = -n + '0';
 		write(fd, &digit, sizeof(char));
+		(*chars_printed) += 1;
 	}
+	return (0);
 }
 
-void	ft_putnbr_fd(int n, int fd)
+int	ft_putnbr_fd(int n, int fd)
 {
+	int	chars_printed;
+
+	chars_printed = 0;
 	if (n < 0)
-		write(fd, "-", sizeof(char));
+		chars_printed += write(fd, "-", sizeof(char));
 	if (n > 0)
 		n *= -1;
-	do_putnbr(n, fd);
+	chars_printed += do_putnbr(n, fd, &chars_printed);
+	return (chars_printed);
 }
