9135022dcf2df1bca1ae86f6427aa4a1f8362e70
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 12:20:57 +0000

    add comments

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,13 +6,20 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:55:30 by vmarin            #+#    #+#             */
-/*   Updated: 2024/05/01 18:28:34 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 12:20:18 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ * Converts an uppercase letter to its lowercase equivalent.
+ *
+ * @param c The character to be converted.
+ * @return The lowercase equivalent of the character if it is uppercase,
+ *         otherwise returns the character unchanged.
+ */
 int	ft_tolower(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-		c += 32;
-	return (c);
+    if (c >= 'A' && c <= 'Z')
+        c += 32;
+    return (c);
 }
