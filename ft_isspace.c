28d5a6a65e5bb4e780ef2fde2b955a71931775b4
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 10:50:24 +0000

    add comments

--- ft_isspace.c
+++ ft_isspace.c
@@ -6,15 +6,20 @@
 /*   By: vmarin <vmarin@42london.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 10:21:02 by vmarin            #+#    #+#             */
-/*   Updated: 2024/11/06 10:22:10 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 10:49:53 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
+/*
+ * Checks if the given character is a whitespace character.
+ * 
+ * @param c The character to be checked.
+ * 
+ * @return 1 if the character is a whitespace character, 0 otherwise.
+ */
 int	ft_isspace(char c)
 {
-	if ((c >= 9 && c <= 13) || c == 32)
-		return (1);
-	return (0);
+    if ((c >= 9 && c <= 13) || c == 32)
+        return (1);
+    return (0);
 }
