42a47be1c8ff0d2bda85a2f0f0e21309d6ce0498
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 11:47:23 +0000

    add comments

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,23 +6,29 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:16:02 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/30 17:21:03 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 11:45:27 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stddef.h>
 
+/*
+ * Outputs the string 's' to the given file descriptor.
+ *
+ * @param s The string to output.
+ * @param fd The file descriptor on which to write.
+ */
 void	ft_putstr_fd(char *s, int fd)
 {
-	size_t	i;
+    size_t	i;
 
-	if (!s)
-		return ;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		write(fd, s + i, sizeof(char));
-		i++;
-	}
+    if (!s)
+        return ;
+    i = 0;
+    while (s[i] != '\0')
+    {
+        write(fd, s + i, sizeof(char));
+        i++;
+    }
 }
