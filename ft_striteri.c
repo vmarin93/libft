5cbdb8993768a37bb626245829ca033c10aa3224
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 12:02:15 +0000

    add comments

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,22 +6,31 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:27:17 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/30 17:28:30 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 12:01:28 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/*
+ * Applies the function 'f' to each character of the string 's',
+ * passing its index as the first argument.
+ * This is similar to the behavior of 'enumerate' in Python,
+ * where both the index and the character are tracked.
+ *
+ * @param s The string on which to iterate.
+ * @param f The function to apply to each character.
+ */
 void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
-	size_t	i;
+    size_t	i;
 
-	if (!s)
-		return ;
-	i = 0;
-	while (s[i] != '\0')
-	{
-		f(i, s + i);
-		i++;
-	}
+    if (!s)
+        return ;
+    i = 0;
+    while (s[i] != '\0')
+    {
+        f(i, s + i);
+        i++;
+    }
 }
