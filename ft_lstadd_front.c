1e11b731920a5d37b0ac004fc18eea3677b3c24b
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 10:59:50 +0000

    add comments

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,14 +6,20 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 20:42:55 by vmarin            #+#    #+#             */
-/*   Updated: 2024/05/01 20:51:01 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 11:00:12 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+ * Adds the new node at the beginning of the list.
+ * 
+ * @param lst A pointer to the first node of the list.
+ * @param new_node The node to be added to the list.
+ */
 void	ft_lstadd_front(t_list **lst, t_list *new_node)
 {
-	new_node->next = *lst;
-	*lst = new_node;
+    new_node->next = *lst;
+    *lst = new_node;
 }
