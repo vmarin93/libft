d4933101bb0dc0b016769a2458b8d17dedb8c0dd
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 11:11:38 +0000

    add comments

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -13,24 +13,34 @@
 #include <stddef.h>
 #include "libft.h"
 
-t_list	*ft_lstmap(t_list *lst, void*(*f)(void *), void (*del)(void *))
+/*
+ * Iterates the list 'lst' and applies the function 'f' to the content of each node.
+ * Creates a new list resulting from the successive applications of 'f'.
+ * The 'del' function is used to delete the content of a node if needed.
+ *
+ * @param lst The address of a pointer to a node.
+ * @param f The function to apply to each node's content.
+ * @param del The function used to delete the content of a node if needed.
+ * @return The new list, or NULL if an allocation fails.
+ */
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	t_list	*diff_node;
+    t_list	*new_list;
+    t_list	*diff_node;
 
-	if (!lst || !f || !del)
-		return (NULL);
-	new_list = NULL;
-	while (lst != NULL)
-	{
-		diff_node = ft_lstnew(f(lst->content));
-		if (!diff_node)
-		{
-			ft_lstclear(&new_list, del);
-			return (NULL);
-		}
-		ft_lstadd_back(&new_list, diff_node);
-		lst = lst->next;
-	}
-	return (new_list);
+    if (!lst || !f || !del)
+        return (NULL);
+    new_list = NULL;
+    while (lst != NULL)
+    {
+        diff_node = ft_lstnew(f(lst->content));
+        if (!diff_node)
+        {
+            ft_lstclear(&new_list, del);
+            return (NULL);
+        }
+        ft_lstadd_back(&new_list, diff_node);
+        lst = lst->next;
+    }
+    return (new_list);
 }
