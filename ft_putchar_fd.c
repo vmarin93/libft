89c95c45b0e1e82ddfced238ef63b87156fd88e3
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 11:37:52 +0000

    add comments

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -6,13 +6,19 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:15:22 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/30 17:15:54 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 11:37:27 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+/*
+ * Outputs the character 'c' to the given file descriptor.
+ *
+ * @param c The character to output.
+ * @param fd The file descriptor on which to write.
+ */
 void	ft_putchar_fd(char c, int fd)
 {
-	write(fd, &c, sizeof(char));
+    write(fd, &c, sizeof(char));
 }
