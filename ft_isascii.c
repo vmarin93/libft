ea8d9d9c61ab62500a51835b9980662b4f02a2a7
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 10:48:22 +0000

    add comments

--- ft_isascii.c
+++ ft_isascii.c
@@ -6,13 +6,20 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 14:03:51 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:35:03 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 10:46:19 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ * Checks if the given character is an ASCII character.
+ * 
+ * @param c The character to be checked.
+ * 
+ * @return 1 if the character is an ASCII character, 0 otherwise.
+ */
 int	ft_isascii(int c)
 {
-	if (c >= 0 && c <= 127)
-		return (1);
-	return (0);
+    if (c >= 0 && c <= 127)
+        return (1);
+    return (0);
 }
