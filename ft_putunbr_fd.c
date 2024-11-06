f5ebec9b8dd73cfdd67b80b6d7bb363ab8650aff
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 11:49:56 +0000

    add comments

--- ft_putunbr_fd.c
+++ ft_putunbr_fd.c
@@ -6,35 +6,54 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 17:53:46 by vmarin            #+#    #+#             */
-/*   Updated: 2024/05/09 17:53:47 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 11:49:09 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-static int	do_putunbr(unsigned int n, int fd, int *chars_printed)
-{
-	char	digit;
+/*
+ * Outputs the unsigned integer 'n' to the given file descriptor.
+ *
+ * @param n The unsigned integer to output.
+ * @param fd The file descriptor on which to write.
+ * @return The number of characters printed.
+ */
+int	ft_putunbr_fd(unsigned int n, int fd);
 
-	if (n / 10 != 0)
-	{
-		do_putunbr(n / 10, fd, chars_printed);
-		do_putunbr(n % 10, fd, chars_printed);
-	}
-	else
-	{
-		digit = n + '0';
-		write(fd, &digit, sizeof(char));
-		(*chars_printed) += 1;
-	}
-	return (0);
-}
+/*
+ * Helper function to recursively output the digits of the unsigned integer 'n'.
+ *
+ * @param n The unsigned integer whose digits are to be output.
+ * @param fd The file descriptor on which to write.
+ * @param chars_printed The count of characters printed.
+ * @return 0 on success.
+ */
+static int	do_putunbr(unsigned int n, int fd, int *chars_printed);
 
 int	ft_putunbr_fd(unsigned int n, int fd)
 {
-	int	chars_printed;
+    int	chars_printed;
 
-	chars_printed = 0;
-	chars_printed += do_putunbr(n, fd, &chars_printed);
-	return (chars_printed);
+    chars_printed = 0;
+    chars_printed += do_putunbr(n, fd, &chars_printed);
+    return (chars_printed);
+}
+
+static int	do_putunbr(unsigned int n, int fd, int *chars_printed)
+{
+    char	digit;
+
+    if (n / 10 != 0)
+    {
+        do_putunbr(n / 10, fd, chars_printed);
+        do_putunbr(n % 10, fd, chars_printed);
+    }
+    else
+    {
+        digit = n + '0';
+        write(fd, &digit, sizeof(char));
+        (*chars_printed) += 1;
+    }
+    return (0);
 }
