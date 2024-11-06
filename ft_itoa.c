fdd65a96e77f682c22a6f1fa291e9c36b78f3a7a
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 10:56:27 +0000

    add comments

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,56 +6,74 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 17:08:05 by vmarin            #+#    #+#             */
-/*   Updated: 2024/05/01 18:15:56 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 10:55:24 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 #include "libft.h"
 
-static int	get_int_len(int n)
-{
-	int	len;
+/*
+ * Converts an integer to a null-terminated string.
+ * 
+ * @param n The integer to be converted.
+ * 
+ * @return A pointer to the null-terminated string representing the integer.
+ */
+char	*ft_itoa(int n);
 
-	len = 0;
-	if (n < 0)
-		len++;
-	if (n > 0)
-		n *= -1;
-	if (n == 0)
-		len++;
-	while (n != 0)
-	{
-		n /= 10;
-		len++;
-	}
-	return (len);
-}
+/*
+ * Helper function to calculate the length of the string needed to represent the integer.
+ * 
+ * @param n The integer whose length is to be calculated.
+ * 
+ * @return The length of the string representation of the integer.
+ */
+static int	get_int_len(int n);
 
 char	*ft_itoa(int n)
 {
-	char	*str;
-	int		i;
+    char	*str;
+    int		i;
 
-	str = ft_calloc(get_int_len(n) + 1, sizeof(char));
-	if (str == NULL)
-		return (NULL);
-	if (n < 0)
-	{
-		str[0] = '-';
-	}
-	if (n == 0)
-		str[0] = '0';
-	i = get_int_len(n) - 1;
-	if (n > 0)
-	{
-		n *= -1;
-	}
-	while (n != 0)
-	{
-		str[i] = -(n % 10) + '0';
-		i--;
-		n /= 10;
-	}
-	return (str);
+    str = ft_calloc(get_int_len(n) + 1, sizeof(char));
+    if (str == NULL)
+        return (NULL);
+    if (n < 0)
+    {
+        str[0] = '-';
+    }
+    if (n == 0)
+        str[0] = '0';
+    i = get_int_len(n) - 1;
+    if (n > 0)
+    {
+        n *= -1;
+    }
+    while (n != 0)
+    {
+        str[i] = -(n % 10) + '0';
+        i--;
+        n /= 10;
+    }
+    return (str);
+}
+
+static int	get_int_len(int n)
+{
+    int	len;
+
+    len = 0;
+    if (n < 0)
+        len++;
+    if (n > 0)
+        n *= -1;
+    if (n == 0)
+        len++;
+    while (n != 0)
+    {
+        n /= 10;
+        len++;
+    }
+    return (len);
 }
