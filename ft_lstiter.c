dcad5e481d2bc2b1fedcccd1bfad86a541a8c84a
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 11:09:47 +0000

    add comments

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -13,16 +13,22 @@
 #include "libft.h"
 #include <stddef.h>
 
+/*
+ * Iterates the list 'lst' and applies the function 'f' to the content of each node.
+ *
+ * @param lst The address of a pointer to a node.
+ * @param f The function to apply to each node's content.
+ */
 void	ft_lstiter(t_list *lst, void (*f)(void *))
 {
-	t_list	*current;
+    t_list	*current;
 
-	if (!lst || !f)
-		return ;
-	current = lst;
-	while (current != NULL)
-	{
-		f(current->content);
-		current = current->next;
-	}
+    if (!lst || !f)
+        return ;
+    current = lst;
+    while (current != NULL)
+    {
+        f(current->content);
+        current = current->next;
+    }
 }
