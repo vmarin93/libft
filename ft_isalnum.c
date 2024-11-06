74aec916a4908681bb32be62853e6fef3d7b375c
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 10:45:31 +0000

    add comments

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,14 +6,21 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:34:07 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:34:47 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 10:44:57 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ * Checks if the given character is alphanumeric (a letter or a digit).
+ * 
+ * @param c The character to be checked.
+ * 
+ * @return 8 if the character is alphanumeric, 0 otherwise.
+ */
 int	ft_isalnum(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
-		|| (c >= '0' && c <= '9'))
-		return (8);
-	return (0);
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+        || (c >= '0' && c <= '9'))
+        return (8);
+    return (0);
 }
