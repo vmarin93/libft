80e5188af2bc6b12358f7abdea84ae5169ddc874
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 11:14:22 +0000

    add comments

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -14,14 +14,22 @@
 #include <stddef.h>
 #include "libft.h"
 
+/*
+ * Allocates memory to and returns a new node. The member variable 
+ * 'content' is initialized with the value of the parameter 'content'. 
+ * The variable 'next' is initialized to NULL.
+ *
+ * @param content The content to create the node with.
+ * @return The new node.
+ */
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*new_node;
+    t_list	*new_node;
 
-	new_node = malloc(sizeof(t_list));
-	if (!new_node)
-		return (NULL);
-	new_node->content = content;
-	new_node->next = NULL;
-	return (new_node);
+    new_node = malloc(sizeof(t_list));
+    if (!new_node)
+        return (NULL);
+    new_node->content = content;
+    new_node->next = NULL;
+    return (new_node);
 }
