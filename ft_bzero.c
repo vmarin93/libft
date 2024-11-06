651d44cb084704cdb40399fa8487c2c745a91704
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 12:23:29 +0000

    remove inline comments

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,7 +6,7 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:22:24 by vmarin            #+#    #+#             */
-/*   Updated: 2024/11/06 10:35:25 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 12:22:45 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 
 /*
  * Erases the first n bytes of memory starting at s by setting them to zero.
+ *
+ * @param s The pointer to the memory area to be filled with zeros.
+ * @param n The number of bytes to set to zero.
  */
 void	ft_bzero(void *s, size_t n)
 {
     size_t	i;
 
     i = 0;
-    while (i < n) // Iterate through the first n bytes
+    while (i < n)
     {
-        ((char *)s)[i] = '\0'; // Set each byte to zero
+        ((char *)s)[i] = '\0';
         i++;
     }
 }
