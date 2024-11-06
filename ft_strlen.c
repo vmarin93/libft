df8f7e65cb485175f08b7552e9c0d55758a4f1a0
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 12:10:14 +0000

    add comments

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,18 +6,24 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 15:04:00 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:38:33 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 12:09:20 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/*
+ * Computes the length of the string 's' excluding the terminating null byte ('\0').
+ *
+ * @param s The string whose length is to be computed.
+ * @return The number of characters in the string 's'.
+ */
 size_t	ft_strlen(const char *s)
 {
-	size_t	i;
+    size_t	i;
 
-	i = 0;
-	while (s[i] != '\0')
-		i++;
-	return (i);
+    i = 0;
+    while (s[i] != '\0')
+        i++;
+    return (i);
 }
