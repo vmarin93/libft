166ed7deddc89bfc544c477160b5d3cb2ac4b614
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 11:35:28 +0000

    add comments

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,23 +6,32 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 12:59:43 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:35:41 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 11:34:30 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/*
+ * Copies 'n' bytes from memory area 'src' to memory area 'dest'.
+ * The memory areas must not overlap. Use ft_memmove if the memory areas do overlap.
+ *
+ * @param dest The destination memory area.
+ * @param src The source memory area.
+ * @param n The number of bytes to copy.
+ * @return A pointer to 'dest'.
+ */
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	size_t		i;
+    size_t		i;
 
-	if (dest == NULL && src == NULL)
-		return (dest);
-	i = 0;
-	while (i < n)
-	{
-		((char *)dest)[i] = ((char *)src)[i];
-		i++;
-	}
-	return (dest);
+    if (dest == NULL && src == NULL)
+        return (dest);
+    i = 0;
+    while (i < n)
+    {
+        ((char *)dest)[i] = ((char *)src)[i];
+        i++;
+    }
+    return (dest);
 }
