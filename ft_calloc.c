79bce85a2e19efeb8b39f30ea7de3d463f3f9974
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 10:44:40 +0000

    add comments

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 16:08:37 by vmarin            #+#    #+#             */
-/*   Updated: 2024/05/01 18:33:16 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 10:42:00 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,31 @@
 #include <stdint.h>
 #include <stddef.h>
 
+/*
+ * Allocates memory for an array and initializes all bytes to zero.
+ * 
+ * @param nmemb Number of elements to allocate.
+ * @param size Size of each element.
+ * 
+ * @return Pointer to the allocated memory, or NULL if the allocation fails.
+ */
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void	*ptr;
-	size_t	i;
+    void	*ptr;
+    size_t	i;
 
-	if (SIZE_MAX / nmemb >= size)
-	{
-		ptr = malloc(nmemb * size);
-		if (!ptr)
-			return (NULL);
-		i = 0;
-		while (i < nmemb * size)
-		{
-			((char *)ptr)[i] = 0;
-			i++;
-		}
-		return (ptr);
-	}
-	return (NULL);
+    if (SIZE_MAX / nmemb >= size) // Check for overflow
+    {
+        ptr = malloc(nmemb * size); // Allocate memory
+        if (!ptr)
+            return (NULL);
+        i = 0;
+        while (i < nmemb * size) // Initialize memory to zero
+        {
+            ((char *)ptr)[i] = 0;
+            i++;
+        }
+        return (ptr);
+    }
+    return (NULL); // Return NULL if overflow
 }
