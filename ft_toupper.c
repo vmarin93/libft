dcb08e139a179447742b652aa5e230c6d42a25a7
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 12:21:29 +0000

    add comments

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,13 +6,20 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 18:46:44 by vmarin            #+#    #+#             */
-/*   Updated: 2024/05/01 18:28:39 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 12:21:05 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ * Converts a lowercase letter to its uppercase equivalent.
+ *
+ * @param c The character to be converted.
+ * @return The uppercase equivalent of the character if it is lowercase,
+ *         otherwise returns the character unchanged.
+ */
 int	ft_toupper(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		c -= 32;
-	return (c);
+    if (c >= 'a' && c <= 'z')
+        c -= 32;
+    return (c);
 }
