e287f8b532d35847c52b08e3232f23ef524ec262
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 10:49:06 +0000

    add comments

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,13 +6,20 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 13:20:43 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:35:10 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 10:48:36 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+ * Checks if the given character is a digit (0-9).
+ * 
+ * @param c The character to be checked.
+ * 
+ * @return 2048 if the character is a digit, 0 otherwise.
+ */
 int	ft_isdigit(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (2048);
-	return (0);
+    if (c >= '0' && c <= '9')
+        return (2048);
+    return (0);
 }
