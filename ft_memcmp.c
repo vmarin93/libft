d6e71d6eb933bfb0ac290554b6a22f6513f94e9a
Vasile Marin <138124674+vmarin93@users.noreply.github.com>
2024-11-06 11:32:38 +0000

    add comments

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,12 +6,23 @@
 /*   By: vmarin <vmarin@student.42london.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 18:15:02 by vmarin            #+#    #+#             */
-/*   Updated: 2024/04/23 20:35:36 by vmarin           ###   ########.fr       */
+/*   Updated: 2024/11/06 14:03:12 by vmarin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/*
+ * Compares the first 'n' bytes of memory area 's1' to memory area 's2'.
+ * The comparison is done byte by byte and stops at the first difference.
+ *
+ * @param s1 The first memory area to compare.
+ * @param s2 The second memory area to compare.
+ * @param n The number of bytes to compare.
+ * @return An integer less than, equal to, or greater than zero if 's1'
+ *         is found, respectively, to be less than, to match, or be greater
+ *         than 's2'.
+ */
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
@@ -23,7 +34,5 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 		i++;
 	}
-	if (i < n)
-		return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 	return (0);
 }
